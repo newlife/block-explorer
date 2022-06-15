4698972ca40520b9a73ba32953a3f11d441fb67b
newlife <shfeng819@126.com>
2022-06-16 02:08:41 +0800

    Initial commit

---
+++ src/components/shared/Footer.vue
@@ -0,0 +1,25 @@
+<script lang="ts" setup>
+import { ref } from 'vue';
+
+const year = ref(new Date().getFullYear());
+</script>
+<template>
+    <div class="footer text-primary-dark dark:text-ternary-light">
+        @{{ year }}
+        <a href="https://github.com/yugasun/vue-template" target="_blank">
+            Github
+        </a>
+        |
+        <a href="https://vitejs.dev/guide/features.html" target="_blank">
+            Vite Docs
+        </a>
+        |
+        <a href="https://v3.vuejs.org/" target="_blank">Vue 3 Docs</a>
+    </div>
+</template>
+<style lang="scss" scoped>
+.footer {
+    line-height: 60px;
+    font-size: 16px;
+}
+</style>
